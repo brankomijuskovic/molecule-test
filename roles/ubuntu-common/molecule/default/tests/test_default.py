be1e6e4bf0c480a71ec22db24e95ca22ac600ec1
root <root@branko-docker.localdomain>
2018-08-24 14:52:27 +0000

    Added debian as test image as well

--- roles/ubuntu-common/molecule/default/tests/test_default.py
+++ roles/ubuntu-common/molecule/default/tests/test_default.py
@@ -9,7 +9,7 @@ def test_htop_is_installed(host):
 	assert host.package("htop").is_installed
 
 def test_vim_is_there(host):
-	assert host.package("vim").is_installed
+	assert host.package("vim-common").is_installed
 
 def test_nginx_is_running(host):
 	assert host.service("nginx").is_running
