69df3915f03eb05ca1cc3c674451a5c860eb99f0
root <root@branko-docker.localdomain>
2018-08-24 14:04:27 +0000

    Updating molecule tests to match ubuntu docker image

--- roles/ubuntu-common/molecule/default/tests/test_default.py
+++ roles/ubuntu-common/molecule/default/tests/test_default.py
@@ -6,11 +6,10 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 def test_htop_is_installed(host):
-	assert host.package("epel-release").is_installed
 	assert host.package("htop").is_installed
 
 def test_vim_is_there(host):
-	assert host.package("vim-enhanced").is_installed
+	assert host.package("vim").is_installed
 
 def test_nginx_is_running(host):
 	assert host.service("nginx").is_running
